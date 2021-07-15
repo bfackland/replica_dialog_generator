552231ec94569821958a300ce6a85da3ba89c3d9
Ben A <ben.a@webappservices.com>
2021-07-15 12:32:36 +0100

    initial commit of utility script to generate dialog audio files from Rasa responses.yml using Replica API

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # replica-dialog-generator
+
+Generating Dialog using Replica API
+-----------------------------------
+
+First you'll need some local Replica API credentials:
+
+```
+# replica_api_credentials.json
+{
+  "client_id" : "<your replica username>",
+  "secret" : "<your replica password>"
+}
+```
+
+Then run `generate_dialog.py` to work through all the dialog in `responses.yml` and
+attempt to generate OGG audio files for each:
+
+```
+python generate_dialog.py
+```
---
+++ dialog.py
@@ -0,0 +1,77 @@
+from loguru import logger
+import yaml
+import hashlib
+from os import listdir
+import json
+
+
+DIALOG_PATH = "../dialog/"
+DIALOG_FILES = [ x for x in listdir(DIALOG_PATH) if x.lower().endswith('.ogg')]
+REPLICA_CONFIG_FILENAME = "./replica_config.json"
+
+
+class Dialog:
+    def __init__(self):
+        replica_config = self.load_replica_config()
+        self.replica_voice_uids = replica_config['replica_voice_uids']
+
+    def load_replica_config(self):
+        """
+        load the project-specific Replica configuration from a json file
+        """
+        logger.info(f"Using Replica Config from {REPLICA_CONFIG_FILENAME}")
+        with open(REPLICA_CONFIG_FILENAME, 'r') as rcfile:
+            replica_config = json.loads(rcfile.read())
+        return replica_config
+
+    def normalise_text(self, text):
+        """
+        converts multi-line text with multiple spaces into one string that
+        is more consistent
+        """
+        text = text.strip()
+        text = text.replace('\n', ' ')
+        while '  ' in text:
+            text = text.replace('  ', ' ')
+        return text
+
+    def load_responses(self):
+        """
+        loads the dialog responses data structure from yaml and returns
+        in a lookup-table format (text_key, text)
+        """
+        responses = {}
+        logger.info("Reading responses from disk...")
+        with open(f'{DIALOG_PATH}/responses.yml', 'r') as file:
+            data = yaml.safe_load(file)
+            if 'responses' in data.keys():
+                for k, v in data['responses'].items():
+                    responses[k] = self.normalise_text(v[0]['text'])
+        logger.info(f"Loaded {len(responses)} responses.")
+        return responses
+
+    def get_text_md5(self, text):
+        """
+        compute the md5 hash of a (normalised) text string
+        """
+        text = self.normalise_text(text)
+        text_md5 = hashlib.md5(text.encode('ascii')).hexdigest()
+        return text_md5
+
+    def get_dialog_file_for_text(self, text):
+        """
+        look to see if we have a dialog file that matches the hash of the
+        text that we want to say
+        """
+        text_md5 = self.get_text_md5(text)
+        dialog_file = [ x for x in DIALOG_FILES if text_md5 in x ]
+
+        if dialog_file:
+            if len(dialog_file) == 1:
+                dialog_file = dialog_file[0]
+            elif len(dialog_file) > 1:
+                logger.warning(f"""Multiple dialog files found for text "{text}" (hash {text_md5})""")
+        else:
+            logger.info(f"""No dialog file found for hash {text_md5}""")
+
+        return dialog_file
